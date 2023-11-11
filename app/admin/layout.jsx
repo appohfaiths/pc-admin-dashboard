db43912baf659c6503ebce8f1b5ad3781cdd51c5
KodjoCode <appohfaiths@gmail.com>
2023-11-11 11:10:50 +0000

    server side route protection

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,11 +1,17 @@
 'use client';
 import isAuth from '../../components/Authentication/isAuth'
+import { useSelector } from "react-redux";
+import { redirect } from "next/navigation";
 
 import Sidebar from '../../components/sidebar';
 import TopBar from '../../components/topBar';
 
- const AdminLayout = ({ children }) => {
+const AdminLayout = ({ children }) => {
+   const isAuth = useSelector((state) => state.auth.isAuthenticated);
   
+   if(!isAuth) {
+        redirect("/");
+    }
   return (
     <main>
       <TopBar />
@@ -17,4 +23,5 @@ import TopBar from '../../components/topBar';
   );
 }
 
-export default isAuth(AdminLayout);
+export default AdminLayout;
+// export default isAuth(AdminLayout); using HOC for protected routes
